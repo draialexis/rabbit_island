9db56cf7cc568081bd2e7fbcbe41dabba9f563b8
Alexis DRAI <drai.alexis@gmail.com>
2022-03-05 17:08:05 +0100

    Do 1): simple fibonacci, naive pop growth

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("yo yo yo");
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        // 1)
+
+        long pop;
+        for (int i = 1; i <= 240; i++)
+        {
+            pop = SimpleRabbitSim.popByMonth(i);
+            if (pop > 8_000_000_000L)
+            {
+                System.out.println("\nOk, that's enough rabbits");
+                break;
+            }
+        }
+
+        //2)
+
+
+
     }
 }
---
+++ src/SimpleRabbitSim.java
@@ -0,0 +1,36 @@
+public class SimpleRabbitSim
+{
+    /**
+     * popByMonth
+     * determines the size of a rabbit population, in couples
+     * @param months
+     * @return
+     */
+    static long popByMonth(int months)
+    {
+        long m_1, m_2, current = 0;
+        int i;
+        // step1: take a couple of baby rabbits
+        m_1 = 1;
+        // step2: wait a month, the couple is now mature for reproduction
+        m_2 = 1;
+        if (months <= 0) throw new IllegalArgumentException("fibo(x) | x<=0 = N/A");
+        if (months == 1 || months == 2)
+        {
+            System.out.println("Month: " + months + " | Pop: " + 1);
+            // System.out.print(1 + ", ");
+            return 1;
+        }
+        for (i = 3; i <= months; i++)
+        {
+            // step3: do the math
+            current = m_1 + m_2;
+            m_2 = m_1;
+            m_1 = current;
+        }
+        // step4: display
+        System.out.println("Month: " + months + " | Pop: " + current);
+        // System.out.print(current + ", ");
+        return current;
+    }
+}
